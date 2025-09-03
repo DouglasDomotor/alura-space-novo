f62b7a89d862e560a70bb4edbf2a006393a3cc5f
Douglas Domotor <dmtr82do@gmail.com>
2025-09-02 21:56:18 -0300

    implantando arquivos static

--- setup/settings.py
+++ setup/settings.py
@@ -101,9 +101,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
 
