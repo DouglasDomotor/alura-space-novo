1d85a540b79e379378ab6156de875fba71a8b937
Douglas Domotor <dmtr82do@gmail.com>
2025-09-19 01:02:28 -0300

    Adicionar ícones de login e cadastro

---
+++ usuarios/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+
+class LoginForms(forms.Form):
+        nome_login = forms.CharField(
+                label='Nome de Login',
+                required=True,
+                max_length=100,
+                widget=forms.TextInput(
+                        attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Ex.: João Silva',
+                        }
+                )
+        )  
+        senha = forms.CharField(
+                label='Senha',
+                max_length=100,
+                required=True,
+                widget=forms.PasswordInput(
+                        attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Digite sua senha',
+                        }                        
+                ),
+        )
+
+class CadastroForms(forms.Form):
+        nome_login = forms.CharField(
+                label='Nome de Login',
+                required=True,
+                max_length=100,
+                widget=forms.TextInput(
+                        attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Ex.: João Silva',
+                        }
+                )
+        )
+        email = forms.EmailField(
+                label='E-mail',
+                required=True,
+                max_length=100,
+                widget=forms.EmailInput(
+                        attrs={
+                                'class': 'form-control',
+                                'placeholder': 'jaoa@xpto.com',
+                        }
+                )
+        )  
+        senha = forms.CharField(
+                label='Senha',
+                max_length=100,
+                required=True,
+                widget=forms.PasswordInput(
+                        attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Senha',
+                        }
+                ),
+        )
+        senha2 = forms.CharField(
+                label='Confirmação de Senha',
+                max_length=100,
+                required=True,
+                widget=forms.PasswordInput(
+                        attrs={
+                                'class': 'form-control',
+                                'placeholder': 'Comfirme a senha',
+                        }
+                ),
+        )
