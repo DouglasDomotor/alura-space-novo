7e849b6576134a609b62cf3731270a00ad3f3b1b
Douglas Domotor <dmtr82do@gmail.com>
2025-09-19 19:42:06 -0300

    Configuração do cadastro de usuários com salvamento no banco de dados

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.contrib.auth.models import User
 
 from .forms import LoginForms, CadastroForms        
 
@@ -8,4 +10,28 @@ def login(request):
 
 def cadastro(request):
     form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            if form['senha1'].value() != form['senha2'].value():
+                return redirect ('cadastro')
+            
+            nome=form['nome_login'].value()
+            email=form['email'].value()
+            senha=form['senha1'].value()
+
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+             
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            return redirect('login')
+
+        
     return render(request, 'usuarios/cadastro.html', {'form': form})
